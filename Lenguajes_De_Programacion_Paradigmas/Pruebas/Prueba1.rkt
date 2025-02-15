48c41ea07600bc3408d06ca3460d3083e52d1f58
Juan Luis Rivera <juanluisrivera@ciencias.unam.mx>
2025-02-15 13:13:13 -0600

    Se agrega Prueba

--- Lenguajes_De_Programacion_Paradigmas/Pruebas/Prueba1.rkt
+++ Lenguajes_De_Programacion_Paradigmas/Pruebas/Prueba1.rkt
@@ -1,2 +1,13 @@
+( define ( suma-simple x y)
+    (+ x y))
+
+( define ( resta-simple x y)
+    (- x y))
+
+
+( define ( three? x )
+    (if(x != 3)#t #f))
+
+
 ( define ( promedio-3 x y z )
     (/ (+ x y z ) 3))
